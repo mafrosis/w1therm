51b47baac02e3248e2494b783d30cabb708a2bc9
Matt Black <github@mafro.net>
2018-04-22 18:31:29 +1000

    Use dnspython to find LAN IP address of jorg

--- dht22.py
+++ dht22.py
@@ -4,10 +4,26 @@ import datetime
 import time
 
 import Adafruit_DHT
+import dns.resolver
 import requests
 
 
+def init():
+    while True:
+        # discover jorg ip address via nslookup
+        res = dns.resolver.Resolver()
+        res.nameservers = ['192.168.1.1']
+
+        try:
+            return [n for n in res.query('jorg.eggs')][0].address
+        except Exception as e:
+            print(e)
+            time.sleep(60)
+
+
 def main():
+    jorg_addr = init()
+
     while True:
         hum, temp = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 25)
 
@@ -18,7 +34,7 @@ def main():
             'temperature': temp,
             'humidity': hum,
         }
-        requests.post('http://jorg.eggs:8003/api/dht22/', data=payload)
+        requests.post('http://{}:8003/api/dht22/'.format(jorg_addr), json=payload)
         time.sleep(60)
 
 
